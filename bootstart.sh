4b5c73f06f32d57b72c798039807eba1b393741f
ran.zhou <ran.zhou@aptiv.com>
2021-11-24 16:06:13 +0800

    Update README

--- bootstart.sh
+++ bootstart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 把开机启动 service 添加到 /etc/systemd/system/ 目录下
-sudo cp network_auto_auth.service /etc/systemd/system/network_auto_auth.service
+# sudo cp network_auto_auth.service /etc/systemd/system/network_auto_auth.service
 
 sudo systemctl stop network_auto_auth.service
 sudo systemctl enable network_auto_auth.service
